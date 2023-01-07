16bc61fc6349496466914500db967afd1f1cab5e
Brax Wong <braxwong@Braxs-MacBook-Pro.local>
2023-01-07 21:20:08 +0000

    docs: Added documentations for admin.h functions.

--- admin.h
+++ admin.h
@@ -6,7 +6,18 @@
 #include <string>
 #include "fileSystem.c++"
 
+//@createAdmin()
+//@description: Will ask for both username and password. 
+//Then both info will be sent and stored within the adminRecord.csv
+//@Helper_Functions: addNewAdmin() -> fileSystem.c++
+
 void createAdmin();
+
+//@adminLogin()
+//@description: Will ask for both username and password. Then check with the database to see if
+//The admin exists, the password is correct, the admin does not exist, the password is incorrect
+//The function will exit after 3 failed attempts.
+//@Helper_Functions: checkAdminLogin() -> fileSystem.c++
 
 void adminLogin();
 
